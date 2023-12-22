b694f8529ac0a767645ce96f6bee7bac099c9f7d
MrKerim <mr.kerimslck@gmail.com>
2023-12-22 23:33:05 +0300

    Q5 on data structures training set

---
+++ algoleague_advance/data_structures_training/Q5/mr_olympia.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int32_t main(){
+    int n; cin>>n;
+    stack<string> exc;
+    for(int i=0;i<n;i++){
+        string input; cin>>input;
+        if(exc.empty()) exc.push(input);
+        else{
+            if(exc.top()==input)exc.pop();
+            else exc.push(input);
+        }
+}
+vector<string> result;
+int s = exc.size();
+    for (int i = 0; i < s; i++)
+    {
+        result.push_back(exc.top());
+        exc.pop();
+    }
+    for (int i = s -1;i>=0 ;i--)
+    {
+        cout<<result[i]<<" ";
+    }
+    
+    
+}
