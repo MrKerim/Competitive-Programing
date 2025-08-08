c4356b649fd7a67b8d30a029c34d71e76d8d6fca
MrKerim <mr.kerimslck@gmail.com>
2025-08-08 09:34:40 +0300

    Primary Department for Each Employee - LeetCode

---
+++ LeetCode/Primary Department for Each Employee - LeetCode/solution.sql
@@ -0,0 +1,13 @@
+-- Write your PostgreSQL query statement below
+SELECT * FROM
+(
+    SELECT employee_id, department_id FROM
+    Employee WHERE primary_flag = 'Y'
+) 
+UNION ALL
+(
+    SELECT employee_id, MIN(department_id) AS department_id FROM
+    Employee
+    GROUP BY employee_id
+    HAVING COUNT(department_id) = 1
+)
